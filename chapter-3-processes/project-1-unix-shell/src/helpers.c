788418624470776bcc1086341c70ad43c7557449
Asad Ali <42726454+dmp990@users.noreply.github.com>
2025-02-23 20:42:24 +0000

    Extract out helpers

--- chapter-3-processes/project-1-unix-shell/src/helpers.c
+++ chapter-3-processes/project-1-unix-shell/src/helpers.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/wait.h>
-#include <ctype.h>
-#include <unistd.h>
 
+#include "helpers.h"
 
 #define MAX_TOKENS 100  // Maximum number of tokens
 
@@ -56,50 +54,4 @@ void free_tokens(char **tokens) {
         free(tokens[i]);
     }
     free(tokens);
-}
-
-int main(int argc, char *argv[])
-{
-    int should_run = 1;
-    while (should_run)
-    {
-        printf("shell>");
-        fflush(stdout);
-
-        char command[100];
-        fgets(command, sizeof(command), stdin);
-
-        int count = 0;
-        char **tokens = split_by_whitespace(command, &count);
-
-        /**
-        * After reading user input, the steps are:
-        * (1) fork a child process using fork()
-        * (2) the child process will invoke execvp()
-        * (3) parent will invoke wait() unless command included &
-        */
-        pid_t pid = fork();
-        if (pid == 0)
-        {
-            // Child Process
-            if (execvp(tokens[0], tokens) == -1)
-            {
-                perror("execvp failed\n");
-            }
-        }
-        else if (pid > 0)
-        {
-            // Parent process
-            printf("Parent process. Wating for child with pid %d to end\n", pid);
-            wait(NULL);
-        } else {
-            // Error
-            printf("Error occurred\n");
-        }
-
-
-        // Free allocated memory
-        free_tokens(tokens);
-    }
-    return 0;
 }
---
+++ chapter-3-processes/project-1-unix-shell/src/helpers.h
@@ -0,0 +1,8 @@
+#ifndef HELPERS_H
+#define HELPERS_H
+
+// prototypes
+char **split_by_whitespace(const char *input, int *count);
+void free_tokens(char **tokens);
+
+#endif // HELPERS_H
